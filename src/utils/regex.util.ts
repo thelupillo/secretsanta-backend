aaa78a3556333111156894cb51abe29e9f76acf7
Gonzalo Lopez <thelupillo@hotmail.com>
2022-07-03 12:38:11 -0500

    Fixed some problems with the regex function

--- src/utils/regex.util.ts
+++ src/utils/regex.util.ts
@@ -1,4 +1,4 @@
-// Username: Has to be in lowercase and between 5 to 15 characters, can have underlines but not in the begining
-export const regexUsername: RegExp = /^[^_][a-z0-9_]{4,14}$/g;
+// Username: Has to be in lowercase and between 5 to 15 characters, can have underlines
+export const regexUsername: RegExp = new RegExp("^[a-z0-9_]{5,15}$");
 // Passcode: Has to be between 4 to 6 digits
-export const regexPasscode: RegExp = /^[0-9]{4,6}$/g;
+export const regexPasscode: RegExp = new RegExp("^[0-9]{4,6}$");
